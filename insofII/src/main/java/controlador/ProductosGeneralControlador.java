b5252e2957d0939707a6143dcb1f07a27eb7f00b
Davidmoref <dmoref00@estudiantes.unileon.es>
2024-06-15 12:00:31 +0200

    Añadido Mostrar todos los productos

--- insofII/src/main/java/controlador/ProductosGeneralControlador.java
+++ insofII/src/main/java/controlador/ProductosGeneralControlador.java
@@ -5,9 +5,14 @@
  */
 package controlador;
 
+import EJB.productosFacadeLocal;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import modelo.productos;
 
 /**
  *
@@ -15,8 +20,34 @@ import javax.inject.Named;
  */
 @Named  //Indicar que la clase esta asociada con un EJB
 @ViewScoped //Indicar que solo existe mientras exista la vista asociada
-public class ProductosGeneralControlador implements Serializable{
-    
+public class ProductosGeneralControlador implements Serializable {
+
+    @EJB
+    private productosFacadeLocal productoEJB;
+
+    private List<productos> listaProductos;
+
     //obtener la lista de productos global, no relacionada con el usuario
+    @PostConstruct
+    public void init() {
+        try {
+            listaProductos = productoEJB.findAll();
+        } catch (Exception e) {
+            e.printStackTrace(); // Imprime la excepción en el log
+        }
+    }
+
+public List<productos> getListaProductos() {
+        return listaProductos;
+    }
+
+    public productosFacadeLocal getProductoEJB() {
+        return productoEJB;
+    }
+
+    public void setProductoEJB(productosFacadeLocal productoEJB) {
+        this.productoEJB = productoEJB;
+    }
+
     
 }
